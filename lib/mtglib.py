26f9bc6266913e668c4b1a78d31f2228b65eb765
dyeadal <dyeadal@gmial.com>
2025-06-28 20:53:32 -0500

    added function to likely be built by contributors

--- lib/mtglib.py
+++ lib/mtglib.py
@@ -1,3 +1,10 @@
 # Functions for Archidekt data, and Scryfall API
 
-#
+# Function to pull data about a card
+def PullCardFromScryfall(card_name):
+
+    # if card contains power, toughness (likely a creature)
+    return # name, cast_cost, card_type, card_description, power, toughness
+
+    # if card does not return power, toughness
+    return # name, cast_cost, card_type, card_description
